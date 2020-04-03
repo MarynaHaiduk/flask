27d96ed5fe6b6c8c97d3497a3426ff3d426d112f
Maryna Haiduk <maryna.haiduk@gmail.com>
2020-04-03 03:24:38 -0400

    flask

--- forms.py
+++ forms.py
@@ -1,15 +1,15 @@
-from wtforms import SubmitField, BooleanField, StringField, TextField, PasswordField, validators
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextField,  validators
 from wtforms.validators import DataRequired, Length
 
 class RegistrationForm(FlaskForm):
-    username = StringField('First Name', [validators.DataRequired(), validators.Length(min=4, max=20)])
-    email = StringField('Email Address', [validators.DataRequired(), validators.Email(), validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
+    username = StringField('Name', [validators.DataRequired(), validators.Length(min=2, max=20)])
+    email = StringField('Email', [validators.DataRequired(), validators.Email(), validators.Length(min=6, max=35)])
+    password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
+        validators.EqualTo('Confirm Password', message='Passwords must match')
     ])
-    confirm = PasswordField('Repeat Password')
+    confirm_password = PasswordField('Repeat Password')
     accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)',
                               [validators.DataRequired()])
     submit = SubmitField('Submit')
--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
 from data_articles import Articles
 from forms import RegistrationForm
+import os # to generate some random sekret key
 
 app = Flask(__name__)
 
+# to generate some random sekret key
+my_sekret_key = os.urandom(24)
+app.config['SECRET_KEY'] = 'X\x18#L\xeb\xda\xe6nc\x19\xfdU_?\x02\xd4\xbf\xda\xd9\x8b0\x1e?\x8d'
+
 Articles = Articles()
-app.secret_key = 'development key'
 
 @app.route('/')
 def home():
@@ -23,10 +28,18 @@ def article(id):
 def about():
     return render_template("about.html")
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/register', methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     return render_template('login/register.html', form=form)
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    form = RegistrationForm()
+    if form.is_submitted():
+        result=request.form
+        return render_template('sucess.html', result=result)
+    return render_template('login/signup.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug = True)
